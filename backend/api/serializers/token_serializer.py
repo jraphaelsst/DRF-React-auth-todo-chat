9f0250fb22cc43db009956d7682e56bdac9b2afa
jraphaelsst <joaoraphaelsst@gmail.com>
2024-01-02 23:06:22 +1100

    serializers added

---
+++ backend/api/serializers/token_serializer.py
@@ -0,0 +1,17 @@
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+
+        token['username'] = user.username
+        token['email'] = user.email
+        
+        token['full_name'] = user.profile.full_name
+        token['bio'] = user.profile.bio
+        token['image'] = user.profile.image
+        token['verified'] = user.profile.verified
+        
+        return token
