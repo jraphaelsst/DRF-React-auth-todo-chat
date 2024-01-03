50ebd39edaf83e37d42c822436f59491beb8f8eb
jraphaelsst <joaoraphaelsst@gmail.com>
2024-01-04 05:12:33 +1100

    added Todo listing

--- backend/api/serializers/__init__.py
+++ backend/api/serializers/__init__.py
@@ -1,3 +1,4 @@
 from .register_serializer import RegisterSerializer
+from .todo_serializer import TodoSerializer
 from .token_serializer import MyTokenObtainPairSerializer
 from .user_serializer import UserSerializer
